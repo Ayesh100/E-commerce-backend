40d423fbf75f21af5e3c048fa88e357bca3072a2
Ayesh100 <117099211+Ayesh100@users.noreply.github.com>
2025-04-22 14:33:12 +0500

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,18 +3,21 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
-use Fideloper\Proxy\TrustProxies as Middleware; // or use // Symfony\Component\HttpFoundation\Request; depending on your Laravel version
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
     /**
-     * Set this to '*' so all proxies are trusted.
-     * Or put your proxy IPs here if you prefer.
+     * Trust all proxies (or you can list CIDR blocks).
+     *
+     * @var array|string|null
      */
     protected $proxies = '*';
 
     /**
-     * Ensure Laravel uses all forwarded headers, including X-Forwarded-Proto.
+     * Headers that should be used to detect proxies.
+     *
+     * @var int
      */
     protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
