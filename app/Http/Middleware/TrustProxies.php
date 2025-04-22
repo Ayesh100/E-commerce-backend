16d8888c5b786fa093ebd00ab3cf10186bd77f6a
Ayesh100 <117099211+Ayesh100@users.noreply.github.com>
2025-04-22 14:38:37 +0500

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,20 +2,20 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Symfony\Component\HttpFoundation\Request;
 
 class TrustProxies extends Middleware
 {
     /**
-     * Trust all proxies (or you can list CIDR blocks).
+     * Trust all proxies (or specify an array of IPs/CIDRs).
      *
      * @var array|string|null
      */
     protected $proxies = '*';
 
     /**
-     * Headers that should be used to detect proxies.
+     * Use all of the forwarded headers (X-Forwarded-For, X-Forwarded-Proto, etc.).
      *
      * @var int
      */
